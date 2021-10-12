290c3f6e7b3c0c76a78c2b42d63df042de59298b
jhhong0930 <jhhong0930@gmail.com>
2021-10-12 09:21:19 +0900

    add: 예약 기능 추가

---
+++ src/main/java/com/sparta/camp/dto/ReservationRequestDto.java
@@ -0,0 +1,24 @@
+package com.sparta.camp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class ReservationRequestDto {
+
+    private Long userId;
+
+    private Long campId;
+
+    private int count;
+
+    private LocalDateTime checkinDate;
+
+}
